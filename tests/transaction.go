1804e589758d502e11c02fb5b8556a81246658e5
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2022-05-03 16:37:44 +0200

    WIP add transaction generator

--- tests/transaction.go
+++ tests/transaction.go
@@ -6,21 +6,30 @@ import (
 
 // TransactionJob is how a transaction load test is parameterized
 type TransactionJob struct {
-	TransactionDurationMax     time.Duration
-	TransactionDurationMin     time.Duration
-	TransactionTimestampSpread time.Duration
-	MinSpans                   uint64
-	MaxSpans                   uint64
-	NumReleases                uint64
-	NumUsers                   uint64
-	MinBreadCrumbs             uint64
-	MaxBreadCrumbs             uint64
-	BreadcrumbCategories       []string
-	BreadcrumbLevels           []string
-	BreadcrumbsTypes           []string
-	BreadcrumbMessages         []string
-	Measurements               []string
-	Operations                 []string
+	//TransactionDurationMax the maximum duration for a transaction
+	TransactionDurationMax time.Duration `json:"transactionDurationMax,omitempty" yaml:"transactionDurationMax,omitempty"`
+	//TransactionDurationMin the minimum duration for a transaction
+	TransactionDurationMin time.Duration `json:"transactionDurationMin,omitempty" yaml:"transactionDurationMin,omitempty"`
+	//TransactionTimestampSpread the spread (from Now) of the timestamp, generated transactions will have timestamps between
+	//`Now` and `Now-TransactionTimestampSpread`
+	TransactionTimestampSpread time.Duration `json:"transactionTimestampSpread,omitempty" yaml:"transactionTimestampSpread,omitempty"`
+	//MinSpans determines the minimum number of spans generated in a transaction
+	MinSpans uint64 `json:"minSpans,omitempty" yaml:"minSpans,omitempty"`
+	//MaxSpans determines the maximum number of spans generated in a transaction
+	MaxSpans uint64 `json:"maxSpans,omitempty" yaml:"maxSpans,omitempty"`
+	//NumReleases determines the maximum number of unique releases generated in a test
+	NumReleases uint64 `json:"numReleases,omitempty" yaml:"numReleases,omitempty"`
+	//NumUsers determines the maximum number of unique users generated in a test
+	NumUsers uint64 `json:"numUsers,omitempty" yaml:"numUsers,omitempty"`
+	//Min
+	MinBreadcrumbs       uint64   `json:"minBreadcrumbs,omitempty" yaml:"minBreadcrumbs,omitempty"`
+	MaxBreadcrumbs       uint64   `json:"maxBreadcrumbs,omitempty" yaml:"maxBreadcrumbs,omitempty"`
+	BreadcrumbCategories []string `json:"breadcrumbCategories,omitempty" yaml:"breadcrumbCategories,omitempty"`
+	BreadcrumbLevels     []string `json:"breadcrumbLevels,omitempty" yaml:"breadcrumbLevels,omitempty"`
+	BreadcrumbsTypes     []string `json:"breadcrumbsTypes,omitempty" yaml:"breadcrumbsTypes,omitempty"`
+	BreadcrumbMessages   []string `json:"breadcrumbMessages,omitempty" yaml:"breadcrumbMessages,omitempty"`
+	Measurements         []string `json:"measurements,omitempty" yaml:"measurements,omitempty"`
+	Operations           []string `json:"operations,omitempty" yaml:"operations,omitempty"`
 }
 
 // Transaction defines the JSON format of a Sentry transaction,
